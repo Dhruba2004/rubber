9a1339fedb8d4702b6bd8242e8c1721b1b4de0e9
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-11-28 17:02:44 +0530

    fix : some random layout fix

--- src/app/(routes)/dashboard/_components/Sidenav.tsx
+++ src/app/(routes)/dashboard/_components/Sidenav.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
 import SidenavTopSection from "./SidenavTopSection";
 
 const Sidenav = () => {
   
   return (
-    <div className="bg-gray-100 h-screen fixed w-72 border-r p-6">
+    <div className="bg-gray-100 dark:bg-blue-900 h-screen fixed w-72 border-r p-6">
       <SidenavTopSection/>
     </div>
   );
