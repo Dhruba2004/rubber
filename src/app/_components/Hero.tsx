e82abf4478d18d2c9a083c06c37dc2b040c820fe
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-11-28 22:52:46 +0530

    feat : changed hero section bgcolor

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900/80 min-h-screen">
+    <section className="min-h-screen">
       <div className="flex items-baseline justify-center px-3 relative bottom-[-5rem] cursor-pointer">
       <h2 className="border border-black dark:border-white px-3 py-2 rounded-full text-center">See What's New | <span className="text-red-500">AI Diagram</span></h2>
       </div>
      
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center mb-[5rem]">
         <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-8xl font-extrabold sm:text-4xl">
+          <h1 className="md:text-7xl font-extrabold sm:text-4xl">
             Understand User Flow
             <strong className="font-extrabold text-red-700 sm:block">
               {" "}
