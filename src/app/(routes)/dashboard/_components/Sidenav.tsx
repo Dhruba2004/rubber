fe8bbb81b4e6a3953b439564093e9ff4671cc64f
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-11-29 22:02:53 +0530

    fix : removed user prop

--- src/app/(routes)/dashboard/_components/Sidenav.tsx
+++ src/app/(routes)/dashboard/_components/Sidenav.tsx
@@ -4,11 +4,11 @@ import SidenavTopSection from "./SidenavTopSection";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const Sidenav = () => {
-  const {user}=useKindeBrowserClient();
+  // const {user}:any = useKindeBrowserClient();
   
   return (
     <div className="bg-gray-100 dark:bg-gray-900/70 h-screen fixed w-72 border-r p-6">
-      <SidenavTopSection user={user}/>
+      <SidenavTopSection/>
     </div>
   );
 };
