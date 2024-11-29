765b8a693cd6e47d3e224f1c72419c23eed3f03c
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-11-29 21:42:43 +0530

    fix : remove type any from user

--- src/app/(routes)/dashboard/_components/Sidenav.tsx
+++ src/app/(routes)/dashboard/_components/Sidenav.tsx
@@ -4,7 +4,7 @@ import SidenavTopSection from "./SidenavTopSection";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const Sidenav = () => {
-  const {user}:any=useKindeBrowserClient();
+  const {user}=useKindeBrowserClient();
   
   return (
     <div className="bg-gray-100 dark:bg-gray-900/70 h-screen fixed w-72 border-r p-6">
